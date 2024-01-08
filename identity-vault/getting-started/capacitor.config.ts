50ed5cfd60108ebf1464ab4390626fc24dc72816
Ken Sodemann <ken@ionic.io>
2024-01-08 11:54:00 -0600

    chore(): restore config changes that were lost somehow

--- identity-vault/getting-started/capacitor.config.ts
+++ identity-vault/getting-started/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.gettingstartediv',
   appName: 'iv-getting-started',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https',
   },
