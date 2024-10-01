18c6930647edc5cae81c5a60e458acbfd21e3570
Ken Sodemann <ken@ionic.io>
2024-10-01 10:35:22 -0500

    fix(iv): fix the unlock page test

--- identity-vault/custom-passcode/src/app/unlock/unlock.page.spec.ts
+++ identity-vault/custom-passcode/src/app/unlock/unlock.page.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavController } from '@ionic/angular/standalone';
+import { createNavControllerMock } from 'test/mocks';
+import { SessionVaultService } from '../core/session-vault.service';
+import { createSessionVaultServiceMock } from '../core/session-vault.service.mock';
 import { UnlockPage } from './unlock.page';
 
 describe('UnlockPage', () => {
@@ -6,6 +10,10 @@ describe('UnlockPage', () => {
   let fixture: ComponentFixture<UnlockPage>;
 
   beforeEach(() => {
+    TestBed.overrideProvider(NavController, { useFactory: createNavControllerMock }).overrideProvider(
+      SessionVaultService,
+      { useFactory: createSessionVaultServiceMock },
+    );
     fixture = TestBed.createComponent(UnlockPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
