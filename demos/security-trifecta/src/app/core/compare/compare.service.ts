a731924520222e36d90b7033cb1ce4436ae7f271
Ken Sodemann <ken.sodemann@gmail.com>
2024-12-21 16:53:22 -0600

    improve the compare typings

--- demos/security-trifecta/src/app/core/compare/compare.service.ts
+++ demos/security-trifecta/src/app/core/compare/compare.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface BrandNamePair {
+  brand: string;
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CompareService {
-  byName(x1: { name: string }, x2: { name: string }): number {
+  byName(x1: Pick<BrandNamePair, 'name'>, x2: Pick<BrandNamePair, 'name'>): number {
     return this.byStringProp(x1, x2, 'name');
   }
 
-  byBrand(x1: { brand: string }, x2: { brand: string }): number {
+  byBrand(x1: Pick<BrandNamePair, 'brand'>, x2: Pick<BrandNamePair, 'brand'>): number {
     return this.byStringProp(x1, x2, 'brand');
   }
 
-  byBrandAndName(x1: { brand: string; name: string }, x2: { brand: string; name: string }): number {
+  byBrandAndName(x1: BrandNamePair, x2: BrandNamePair): number {
     return this.byBrand(x1, x2) || this.byName(x1, x2);
   }
 
-  private byStringProp(
-    x1: Partial<{ brand: string; name: string }>,
-    x2: Partial<{ brand: string; name: string }>,
-    propName: 'brand' | 'name',
-  ): number {
-    const str1 = x1[propName]?.trim().toUpperCase();
-    const str2 = x2[propName]?.trim().toUpperCase();
+  private byStringProp(x1: Partial<BrandNamePair>, x2: Partial<BrandNamePair>, propName: 'brand' | 'name'): number {
+    const str1 = x1[propName]?.trim().toUpperCase() || '';
+    const str2 = x2[propName]?.trim().toUpperCase() || '';
 
-    return str1?.localeCompare(str2 || '') ?? -1;
+    return str1.localeCompare(str2 || '');
   }
 }
