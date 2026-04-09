949f1957d926646798280c63f70f6ca1b9aeae9e
Marlon Harrison <92547717+marlon-ionic@users.noreply.github.com>
2026-04-09 13:23:09 -0500

    fix(android): normalize bundle ids and application names

---
+++ auth-connect/refresh-workflow/android/app/src/main/java/io/ionic/demos/enterprise/authconnect/refreshworkflow/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.demos.enterprise.authconnect.refreshworkflow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ identity-vault/custom-passcode/android/app/src/main/java/io/ionic/demos/enterprise/identityvault/custompasscode/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.demos.enterprise.identityvault.custompasscode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
